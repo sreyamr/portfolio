eba1d9d2226cfdc15c04f13cac9b233ef0101625
sreyamr <sreyaleya999@gmail.com>
2026-03-27 12:57:13 +0530

    Updates

--- src/components/Contact.jsx
+++
@@ -1,8 +0,0 @@
-export default function Contact() {
-  return (
-    <section id='contact' className='p-10 text-center'>
-      <h2 className='text-3xl font-bold'>Contact</h2>
-      <p className='text-gray-400 mt-4'>Let’s work together 🚀</p>
-    </section>
-  )
-}
--- src/components/Footer.jsx
+++
@@ -1,7 +0,0 @@
-export default function Footer() {
-  return (
-    <footer className='text-center p-6 text-gray-500'>
-      © 2026 Mobile Portfolio
-    </footer>
-  )
-}
